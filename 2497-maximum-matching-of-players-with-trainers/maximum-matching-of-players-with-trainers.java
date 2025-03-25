e87f3b7c04cb81cab9e03d9088582552c9ba56aa
Nanda Gopal <98415072+nandagopal030@users.noreply.github.com>
2025-03-25 17:35:22 +0530

    Time: 26 ms (71.04%) | Memory: 65.6 MB (24.21%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -1,19 +1,41 @@
+// class Solution {
+//     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+//         Arrays.sort(players); Arrays.sort(trainers);   
+//        int count = 0;
+//         // if(trainers.length == 1 && trainers[0] <= players[0]){
+//         //     return 1;
+//         // }
+
+//         int  i =0;
+//         for(int j = 0; i<players.length && j <trainers.length; j++){
+//             if(players[i] <= trainers[j]){
+//                 count++;
+//                 i++;
+//             }
+//         }
+//         return count;
+
+//     }
+// }
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players); Arrays.sort(trainers);   
-       int count = 0;
-        // if(trainers.length == 1 && trainers[0] <= players[0]){
-        //     return 1;
-        // }
+        Arrays.sort(players);
+        Arrays.sort(trainers);
 
-        int  i =0;
-        for(int j = 0; i<players.length && j <trainers.length; j++){
-            if(players[i] <= trainers[j]){
-                count++;
-                i++;
+        int p = 0, t = 0;
+
+        while(p < players.length && t < trainers.length){
+            //Trainer is big
+            if(players[p] <= trainers[t]){
+                p++;
+                t++;
+            }else{
+                //want bigger trainer
+                t++;
             }
+            
         }
-        return count;
-
+        
+        return p;
     }
 }
