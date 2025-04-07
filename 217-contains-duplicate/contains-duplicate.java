964db23b28ea4749bc09f2d3869a5b56222098d8
Nanda Gopal <98415072+nandagopal030@users.noreply.github.com>
2025-04-07 17:38:41 +0530

    Time: 11 ms (70.93%) | Memory: 57.7 MB (69.23%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -7,15 +7,23 @@ class Solution {
         //       }
         // }
         // return false;
-      Arrays.sort(nums);
-      for(int i=0; i<nums.length -1; i++){
-        if(nums[i] == nums[i+ 1]){
+
+    //method 2;
+    //   Arrays.sort(nums);
+    //   for(int i=0; i<nums.length -1; i++){
+    //     if(nums[i] == nums[i+ 1]){
+    //         return true;
+    //     }
+    //   }
+    //   return false;
+    HashSet<Integer> set = new HashSet<>();
+    for(int i= 0; i<nums.length; i++){
+        if(set.contains(nums[i])){
             return true;
+        }else{
+            set.add(nums[i]);
         }
-      }
-      return false;
-
-
-
+    }
+    return false;
     }
 }
