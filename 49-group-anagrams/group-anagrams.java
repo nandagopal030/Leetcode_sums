56a5187b3cd5878bb60c22be19aa114403394472
Nanda Gopal <98415072+nandagopal030@users.noreply.github.com>
2025-04-09 19:37:50 +0530

    Time: 21 ms (12.99%) | Memory: 49 MB (14.74%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a'] += 1;
+            }
+            String k = Arrays.toString(count);
+            map.computeIfAbsent(k, key -> new ArrayList<String>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
