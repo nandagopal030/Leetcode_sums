89636609c85653612688b2a6a3775c9c4ce8413d
Nanda Gopal <98415072+nandagopal030@users.noreply.github.com>
2025-05-01 15:21:26 +0530

    Time: 14 ms (32.97%) | Memory: 44.9 MB (35.41%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            // Skip non-alphanumeric characters on the left
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            // Skip non-alphanumeric characters on the right
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            // Compare characters (case-insensitive)
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                return false;
-            }
-
-            left++;
-            right--;
+     String cleaned = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+    //  System.out.println(cleaned);
+    //  return false;
+    int start = 0;
+    int end = cleaned.length() - 1; 
+    while(start < end){
+        if(cleaned.charAt(start) == cleaned.charAt(end)){
+            start++;
+            end--;
         }
-
-        return true;
+        else{
+            return false;
+        }
+    }
+    return true;
     }
 }
