316f4cf5d25883cb0d961671625cd63c4f357825
Nanda Gopal <98415072+nandagopal030@users.noreply.github.com>
2025-05-09 12:32:13 +0530

    Time: 7 ms (36.23%) | Memory: 44.8 MB (26.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        char[] sChar = s.toCharArray();
-        char[] tChar = t.toCharArray();
-
-        Arrays.sort(sChar);
-        Arrays.sort(tChar);
-        
-        
-        return  Arrays.equals(sChar, tChar);
-
+     if(s.length() != t.length()){
+        return false;
+     }
+     char[] aArray = s.toLowerCase().toCharArray();
+     char[] bArray = t.toLowerCase().toCharArray();
+     Arrays.sort(aArray);
+     Arrays.sort(bArray);
+     if(Arrays.equals(aArray, bArray)) return true;
+     
+     return false;
     }
 }
